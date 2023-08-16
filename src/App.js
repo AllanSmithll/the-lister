667e943298704f219d1ec1b54b420a966cdbcec3
allansmithll <allanammancio2004@gmail.com>
2023-08-15 22:56:07 -0300

    Nova versão com estilização

--- src/App.js
+++ src/App.js
@@ -13,33 +13,47 @@ function App() {
   }, []);
 
   useEffect(() => {
-    localStorage.setItem('tasks', JSON.stringify(tasks))
+    localStorage.setItem('tasks', JSON.stringify(tasks));
   }, [tasks])
 
   const handleAdd = useCallback(() => {
-    setTasks([...tasks, input]);
-    setInput('');
+    if (input !== '') {
+      setTasks([...tasks, input]);
+      setInput('');
+    }
   }, [input, tasks]);
 
   const handleRemove = useCallback(() => {
-    setTasks([]);
+    const tasksStorage = localStorage.getItem('tasks');
+    if (tasksStorage) {
+      setTasks([]);
+    }
   }, []);
 
   return(
-    <div>
-      <ul>
-        {tasks.map(task => (
-          <li key={task}>{task}</li>
-        ))}
-      </ul>
-      <br/>
-      <input type="text" value={input} onChange={e => setInput(e.target.value)}/>
-      <button type='button' onClick={handleAdd}>
-        Adicionar tarefa
-      </button>
-      <button type='button' onClick={handleRemove}>
-        Limpar tarefas
-      </button>
+    <div className='the-lister-container'>
+      <header id='header-container'>
+        <h1 id='title'>The Lister</h1>
+      </header>
+      <main id='main-content'>
+        <ul className='tasks-list'>
+          {tasks.map (task =>
+              <li key={task}>{task}
+                <input type='checkbox' 
+                checked={task.checked}/>
+              </li>
+            )}
+        </ul>
+        <input type="text" value={input} onChange={e => setInput(e.target.value)}/>
+        <div className='buttons-container'>
+          <button type='button' onClick={handleAdd}>
+            Adicionar tarefa
+          </button>
+          <button type='button' onClick={handleRemove}>
+            Limpar tarefas
+          </button>
+        </div>
+      </main>
     </div>
   );
 }
